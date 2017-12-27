5093c3e1fffaeacb794d20544bdc58cea530a4ba
Nicolas Rey <rey.nico@gmail.com>
2017-12-26 21:02:17 -0300

    Noticed some jitter when listening. Trying some process priority

--- opt/check_band.sh
+++ opt/check_band.sh
@@ -5,6 +5,9 @@ then
 else
 	sudo systemctl start websdr@$1.service
 	sudo kill -9 `pidof rtl_tcp`
+	sleep 3
+	sudo renice -10 `pidof rtl_tcp`
+	sudo renice -10 `pidof websdr-rpi`
 	if [ "$1" == "80" ]; then
 		echo 1 > /sys/class/gpio/gpio3/value
 	else
