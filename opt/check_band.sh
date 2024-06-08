02ba8ee5663bf1264f9eed705a336e6709714df3
Nicolas Rey <reynico@gmail.com>
2024-06-08 10:25:18 -0300

    add a safe check for the check_band script

--- opt/check_band.sh
+++ opt/check_band.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+set -e
+
+if [ $# -eq 0 ]; then
+    echo "Usage: ./check_band.sh BAND. example: ./check_band.sh 40"
+fi
+
+BAND="$1"
+
 if pgrep -x "websdr-rpi" > /dev/null
 then
 	echo "Running"
 else
-	sudo systemctl start websdr@$1.service
-	sudo kill -9 `pidof rtl_tcp`
+	sudo systemctl start websdr@${BAND}.service
+	sudo kill -9 $(pidof rtl_tcp)
 	sleep 3
-	sudo renice -10 `pidof rtl_tcp`
-	sudo renice -10 `pidof websdr-rpi`
-	if [ "$1" == "80" ]; then
+	sudo renice -10 $(pidof rtl_tcp)
+	sudo renice -10 $(pidof websdr-rpi)
+	if [ "$BAND" == "80" ]; then
 		echo 1 > /sys/class/gpio/gpio3/value
 	else
 		echo 0 > /sys/class/gpio/gpio3/value
